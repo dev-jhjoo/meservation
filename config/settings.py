6ee79b4f2e16fbc2299a655064cd912dffea81cd
Jonggu Lee <jg.jake.lee@gmail.com>
2023-07-01 11:15:41 +0900

    chore: DB 변경

--- config/settings.py
+++ config/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Local Database Endpoint
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "meservation",
+        "USER": "dev-meservation",
+        "PASSWORD": "1q2w3e4r~!",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
