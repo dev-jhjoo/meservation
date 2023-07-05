f269e0a7ae6ad8bdc6b6a51e40f6206b4dcbf773
Jonggu Lee <jg.jake.lee@gmail.com>
2023-07-05 15:40:46 +0900

    feat: Change user db

--- users/admin.py
+++ users/admin.py
@@ -1,22 +1,40 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from users.models import User
+from django.utils.translation import gettext_lazy as _
 
+class UserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    readonly_fields = ('uuid', 'date_joined', 'last_login')
-    fieldsets = [
-        (None, {"fields": ("username", "password", "uuid")}),
-        ("개인정보", {"fields": ("first_name", "last_name", "email", "date_joined", "last_login", "short_description")}),
-        (
-            "권한",
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                )
-            },
-        ),
-    ]
+class UserCreateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'uuid', 'password', 'email')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'description')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('create_at', 'update_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    readonly_fields = ('create_at','update_at','uuid')
+
+    form = UserChangeForm
+    add_form = UserCreateForm
+    list_display = ('username', 'email', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(User, UserAdmin)
+    
--- users/models.py
+++ users/models.py
@@ -1,11 +1,78 @@
 import uuid
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.db import models 
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
 
-class User(AbstractUser):
-    short_description = models.TextField(blank=True, verbose_name="한 줄 소개")
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, verbose_name="UUID")
+class UserManager(BaseUserManager):
+    use_in_migrations = True
 
-    def __str__(self):
-        return self.username
-  
+    # 유저 생성
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("email은 필수입니다.")
+        if not username:
+            raise ValueError("username은 필수입니다.")
+        if not password:
+            raise ValueError("password는 필수입니다.")
+
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.is_staff = False
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    # 슈퍼유저 생성
+    def create_superuser(self, username, email, password):
+        user = self.create_user(
+            username=username,
+            email=self.normalize_email(email),
+            password=password,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    id = models.AutoField(_("id"), primary_key=True)
+
+    uuid = models.UUIDField(_("UUID"), default=uuid.uuid4, editable=False, unique=True)
+    username = models.CharField(_("username"), max_length=50, validators=[username_validator], unique=True)
+    
+    first_name = models.CharField(_("first name"), max_length=50, blank=True)
+    last_name = models.CharField(_("last name"), max_length=50, blank=True)
+    email = models.EmailField(_("email address"), unique=True)
+    description = models.TextField(_("description"), max_length=50, blank=True)
+
+    is_staff = models.BooleanField(_("staff status"), default=False)
+
+    create_at = models.DateTimeField(_("create_at"), auto_now_add=True)
+    update_at = models.DateTimeField(_("update_at"), auto_now=True)
+    last_login_at = models.DateTimeField(_("last login"), auto_now=True)
+
+    deleted_at = models.BooleanField(_("deleted at"), default=False)
+
+    objects = UserManager()
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    # 관리자 페이지에 표시될 이름
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    # 이메일 정규화 로직
+    def clean(self):
+        super().clean()
+        self.__class__.objects.normalize_email(self.email)
+
