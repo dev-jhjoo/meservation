40b8af41303e0d84aa785c48573cf25cebf7b6e0
Jonggu Lee <jg.jake.lee@gmail.com>
2023-07-21 17:29:07 +0900

    chore: 코드 정리

--- config/settings.py
+++ config/settings.py
@@ -137,9 +137,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
 }
 
 REST_USE_JWT = True
